45a4fa1e99313f9540f1cbc43805a4acc88e4615
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-30 18:57:28 +0530

    [Suraj]: Usecase 1 to push data into stack is added.

---
+++ StackQueue/StackQueue/Program.cs
@@ -0,0 +1,10 @@
+﻿StackQueue.Stack stack = new StackQueue.Stack();
+int choice = 1;
+while (choice != 2)
+{
+    Console.WriteLine("Enter data to add in Stack: ");
+    int data = Convert.ToInt32(Console.ReadLine());
+    stack.addData(data);
+    Console.WriteLine("1.Add\n2.Exit");
+    choice = Convert.ToInt32(Console.ReadLine());
+}
---
+++ StackQueue/StackQueue/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace StackQueue
+{
+    internal class Stack
+    {
+        
+        Node top;
+        public void StackQueue()
+        {
+            this.top = null;
+        }
+        public void addData(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("Data added at the top: "+ data);
+        }
+    }
+}
